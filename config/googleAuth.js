d2fb1b556304f91aa3e25d69fad138745e579076
iyozh <evzhenko1106@gmail.com>
2021-09-19 20:09:27 +0300

    changed scheme on prod

--- config/googleAuth.js
+++ config/googleAuth.js
@@ -5,11 +5,11 @@ const config = require('./mainConfig');
 const HOSTNAME = config.app.host
 const PORT = config.app.port
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser(function (user, done) {
     done(null, user);
 });
 
@@ -17,10 +17,10 @@ passport.use(new GoogleStrategy({
         clientID: config.auth.googleClientID,
         clientSecret: config.auth.googleSecretKey,
         callbackURL: process.env.NODE_ENV === 'prod' ? `http://${HOSTNAME}/auth/google/callback` :
-            `http://${HOSTNAME}:${PORT}/auth/google/callback`
+            `https://${HOSTNAME}:${PORT}/auth/google/callback`
     },
-    function(accessToken, refreshToken, profile, done) {
-            return done(null, profile);
+    function (accessToken, refreshToken, profile, done) {
+        return done(null, profile);
     }
 ));
 
