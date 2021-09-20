bef5c1d8acdd7b89557f1fcc635b1192505ee178
iyozh <evzhenko1106@gmail.com>
2021-09-20 04:15:48 +0300

    refactoring config file

--- config/config.js
+++ config/config.js
@@ -4,19 +4,11 @@ module.exports = {
     "development": {
         "use_env_variable": "DATABASE_URL",
         "url": process.env.DATABASE_URL,
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASS,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
         "dialect": "postgres"
     },
     "production": {
         "use_env_variable": "DATABASE_URL",
         "url": process.env.DATABASE_URL,
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASS,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
         "dialect": "postgres"
     }
 }
