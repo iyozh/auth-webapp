529a33d992e4312c6488acdeb6c828b3e0f09ce8
iyozh <evzhenko1106@gmail.com>
2021-09-20 04:10:28 +0300

    fixing dbconfig file

--- config/config.js
+++ config/config.js
@@ -9,5 +9,14 @@ module.exports = {
         "database": process.env.DB_NAME,
         "host": process.env.DB_HOST,
         "dialect": "postgres"
+    },
+    "production": {
+        "use_env_variable": "DATABASE_URL",
+        "url": process.env.DATABASE_URL,
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASS,
+        "database": process.env.DB_NAME,
+        "host": process.env.DB_HOST,
+        "dialect": "postgres"
     }
 }
