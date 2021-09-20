e148a3cfb5e38fa2d4aac2b60dfb15991500c9f2
iyozh <evzhenko1106@gmail.com>
2021-09-20 14:38:17 +0300

    refactoring config file to connect db on heroku

--- config/config.js
+++ config/config.js
@@ -4,11 +4,17 @@ module.exports = {
     "development": {
         "use_env_variable": "DATABASE_URL",
         "url": process.env.DATABASE_URL,
-        "dialect": "postgres"
+        "dialect": "postgres",
     },
-    "production": {
-        "use_env_variable": "DATABASE_URL",
-        "url": process.env.DATABASE_URL,
-        "dialect": "postgres"
+        "production": {
+            "use_env_variable": "DATABASE_URL",
+            "url": process.env.DATABASE_URL,
+            "dialect": "postgres",
+            "dialectOptions": {
+                "ssl": {
+                    "require": true,
+                    "rejectUnauthorized": false
+                }
+            }
+        }
     }
-}
